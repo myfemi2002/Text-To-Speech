8ce2c9f0d0a76481422291a8aba53315786aac8e
Femi Falana <myfemi2002@yahoo.co.uk>
2022-06-19 14:34:45 +0100

    first commit

---
+++ Text To Speech1/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Text_To_Speech1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtBox = new System.Windows.Forms.TextBox();
+            this.BtnSpeak = new System.Windows.Forms.Button();
+            this.BtnPause = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SoundTrackBar = new System.Windows.Forms.TrackBar();
+            this.SpeedTrackBar = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtnStop = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.PersonComboBox = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.SoundTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SpeedTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TxtBox
+            // 
+            this.TxtBox.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.TxtBox.Location = new System.Drawing.Point(41, 63);
+            this.TxtBox.Multiline = true;
+            this.TxtBox.Name = "TxtBox";
+            this.TxtBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TxtBox.Size = new System.Drawing.Size(664, 208);
+            this.TxtBox.TabIndex = 0;
+            // 
+            // BtnSpeak
+            // 
+            this.BtnSpeak.Location = new System.Drawing.Point(41, 299);
+            this.BtnSpeak.Name = "BtnSpeak";
+            this.BtnSpeak.Size = new System.Drawing.Size(114, 28);
+            this.BtnSpeak.TabIndex = 1;
+            this.BtnSpeak.Text = "Speak";
+            this.BtnSpeak.UseVisualStyleBackColor = true;
+            this.BtnSpeak.Click += new System.EventHandler(this.BtnSpeak_Click);
+            // 
+            // BtnPause
+            // 
+            this.BtnPause.Location = new System.Drawing.Point(171, 299);
+            this.BtnPause.Name = "BtnPause";
+            this.BtnPause.Size = new System.Drawing.Size(114, 28);
+            this.BtnPause.TabIndex = 2;
+            this.BtnPause.Text = "Pause";
+            this.BtnPause.UseVisualStyleBackColor = true;
+            this.BtnPause.Click += new System.EventHandler(this.BtnPause_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(291, 299);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(114, 28);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Resume";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.BtnResume);
+            // 
+            // SoundTrackBar
+            // 
+            this.SoundTrackBar.Location = new System.Drawing.Point(601, 299);
+            this.SoundTrackBar.Maximum = 100;
+            this.SoundTrackBar.Name = "SoundTrackBar";
+            this.SoundTrackBar.Size = new System.Drawing.Size(104, 45);
+            this.SoundTrackBar.TabIndex = 6;
+            this.SoundTrackBar.Value = 75;
+            // 
+            // SpeedTrackBar
+            // 
+            this.SpeedTrackBar.Location = new System.Drawing.Point(362, 12);
+            this.SpeedTrackBar.Minimum = -10;
+            this.SpeedTrackBar.Name = "SpeedTrackBar";
+            this.SpeedTrackBar.Size = new System.Drawing.Size(104, 45);
+            this.SpeedTrackBar.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(553, 307);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Volume";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(318, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Speed";
+            // 
+            // BtnStop
+            // 
+            this.BtnStop.Location = new System.Drawing.Point(411, 299);
+            this.BtnStop.Name = "BtnStop";
+            this.BtnStop.Size = new System.Drawing.Size(114, 28);
+            this.BtnStop.TabIndex = 8;
+            this.BtnStop.Text = "Stop";
+            this.BtnStop.UseVisualStyleBackColor = true;
+            this.BtnStop.Click += new System.EventHandler(this.BtnStop_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(674, 347);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Design by Femi-Blazer";
+            // 
+            // PersonComboBox
+            // 
+            this.PersonComboBox.FormattingEnabled = true;
+            this.PersonComboBox.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.PersonComboBox.Location = new System.Drawing.Point(584, 16);
+            this.PersonComboBox.Name = "PersonComboBox";
+            this.PersonComboBox.Size = new System.Drawing.Size(121, 21);
+            this.PersonComboBox.TabIndex = 10;
+            this.PersonComboBox.Text = "Male";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.ClientSize = new System.Drawing.Size(785, 361);
+            this.Controls.Add(this.PersonComboBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.BtnStop);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SpeedTrackBar);
+            this.Controls.Add(this.SoundTrackBar);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.BtnPause);
+            this.Controls.Add(this.BtnSpeak);
+            this.Controls.Add(this.TxtBox);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Text To Speech Converter";
+            ((System.ComponentModel.ISupportInitialize)(this.SoundTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SpeedTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtBox;
+        private System.Windows.Forms.Button BtnSpeak;
+        private System.Windows.Forms.Button BtnPause;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TrackBar SoundTrackBar;
+        private System.Windows.Forms.TrackBar SpeedTrackBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BtnStop;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox PersonComboBox;
+    }
+}
+
---
+++ Text To Speech1/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Synthesis; 
+
+namespace Text_To_Speech1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SpeechSynthesizer sp = new SpeechSynthesizer();
+        private void BtnSpeak_Click(object sender, EventArgs e)
+        {
+           
+            sp.Dispose();
+            sp = new SpeechSynthesizer ();
+            sp.Rate = SpeedTrackBar.Value;
+            sp.Volume  = SoundTrackBar.Value;
+
+            if (PersonComboBox.Text == "Male")
+            {
+                sp.SelectVoiceByHints(VoiceGender.Male);
+            }
+
+            if (PersonComboBox.Text == "Female")
+            {
+                sp.SelectVoiceByHints(VoiceGender.Female);
+
+            }
+
+            sp.SpeakAsync (TxtBox.Text);
+
+            if (TxtBox.Text == "")
+            {
+                MessageBox.Show("Enter Some Text First!");
+                return;
+            }
+     }
+
+        private void BtnPause_Click(object sender, EventArgs e)
+        {
+            if (sp.State== SynthesizerState.Speaking )
+
+        {
+                 sp.Pause();
+             }
+        }
+
+        private void BtnResume(object sender, EventArgs e)
+        {
+            if (sp.State == SynthesizerState.Paused)
+            {
+                sp.Resume();
+            }
+        }
+
+        private void BtnStop_Click(object sender, EventArgs e)
+        {
+            if (sp.State == SynthesizerState.Speaking)
+            {
+                sp.Dispose();
+            }
+        }
+    }
+}
